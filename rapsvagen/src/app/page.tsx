e90e412db13ab9e332d610b103aed6f08452b97e
Nelly <no222hh@student.lnu.se>
2025-04-28 21:55:05 +0200

    addning files

---
+++ rapsvagen/src/app/page.tsx
@@ -0,0 +1,17 @@
+// pages/index.js
+import React from 'react';
+
+
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Välkommen till Rapsvägen!</h1>
+      <p>Denna hemsida är under konstruktion.</p>
+      <p>Vi jobbar på att få den klar så snart som möjligt.</p>
+      <footer>© 2025 Rapsvägen</footer>
+    </div>
+  );
+};
+
+export default Home;
