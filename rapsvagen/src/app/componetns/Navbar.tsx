b1ee7a969c6cde7c331b2f4c68b42c6a9fc6b4cc
Nelly <no222hh@student.lnu.se>
2025-05-01 20:59:07 +0200

    adding pages

---
+++ rapsvagen/src/app/componetns/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full p-5 bg-black text-white backdrop-blur-md flex justify-between items-center z-10 shadow-lg relative">
+      <h2 className="text-xl font-bold">Rapsvägen</h2>
+      <div className="space-x-6 text-base">
+        <Link href="/" className="hover:underline font-bold">Hem</Link>
+        <Link href="/galleri" className="hover:underline font-bold">Galleri</Link>
+        <Link href="/om" className="hover:underline font-bold">Om</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- rapsvagen/src/app/page.tsx
+++ rapsvagen/src/app/page.tsx
@@ -1,17 +1,47 @@
-// pages/index.js
 import React from 'react';
+import Link from 'next/link';
+import Navbar from "./componetns/Navbar";
+import Image from 'next/image';
 
-
-
-const Home = () => {
+export default function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Välkommen till Rapsvägen!</h1>
-      <p>Denna hemsida är under konstruktion.</p>
-      <p>Vi jobbar på att få den klar så snart som möjligt.</p>
-      <footer>© 2025 Rapsvägen</footer>
-    </div>
-  );
-};
+    <main className="relative min-h-screen font-sans">
+      {/* Bakgrundsbilden ligger bakom med z-index */}
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src="/white.jpg"
+          alt="bakgrund"
+          fill
+          style={{ objectFit: "cover" }}
+          quality={100}
+          priority
+        />
+      </div>
 
-export default Home;
+      <Navbar />
+
+      <div className="text-center min-h-screen p-10 bg-yellow-50/60">
+        <h1 className="text-4xl text-green-900 font-bold mb-4">Välkommen till Rapsvägen!</h1>
+        <p className="text-lg text-black m-[10px]">Snart är det dags för årets stora "Cykelfest utan cykel"! 🚴‍♀️🌞</p>
+
+        <p className="text-lg mb-2 text-black">📅 Datum: 9 Augusti 2025</p>
+        <p className="text-lg mb-2 text-black">⏳ Sista anmälningsdag: 30 Juni</p>
+        <p className="text-lg mb-6 text-black">✅ Anmäl dig nu för att säkra din plats!</p>
+
+        {/* Under konstruktion meddelande */}
+       
+          <p className='text-lg mb-6 text-black'>🚧 Denna sida är under konstruktion! Vissa funktioner kan vara otillgängliga.</p>
+        
+
+        {/* Anmälningslänk */}
+        <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeFEXEMPEL/viewform" target="_blank">
+          <span className="inline-block mt-4 px-6 py-3 bg-green-700 text-white font-semibold rounded-xl shadow hover:bg-green-800 transition">
+            Anmäl dig här
+          </span>
+        </Link>
+
+       
+      </div>
+    </main>
+  );
+}
